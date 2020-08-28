672d5688c71fd81ec10289735c9da684b020df48
Tom Dale <thomas.dale2@gmail.com>
2020-08-28 09:03:11 -0700

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -2,26 +2,9 @@
   <div>
     <div class="m-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 gap-4">
       <div v-for="product in products" :key="product.id" v-bind:product="product" class="border rounded-lg bg-gray-100 hover:shadow-lg">
-        <product-widget :product="product" :imgsize="'(min-width: 768px) 240px, 50vw'"></product-widget>
+        <product-widget :product="product"></product-widget>
       </div>
     </div>
-<!-- Old styling
-  <section class="home">
-    <article>
-      <div class="hero" :style="{ backgroundImage: 'url(' + hero + ')' }" ></div>
-      <h1 class="blog-title">
-        {{ $prismic.asText(homepageContent.title) }}
-      </h1>
-      <div v-if="products.length !== 0" class="blog-main">
-        <section v-for="product in products" :key="product.id" v-bind:product="product" class="blog-post">
-          <product-widget :product="product" :imgsize="'(min-width: 768px) 700px, 90vw'"></product-widget>
-        </section>
-      </div>
-      <div v-else class="blog-main">
-        <p>No Products available at this time.</p>
-      </div>
-    </article>
-  </section> -->
   </div>
 </template>
 
@@ -29,12 +12,14 @@
 // Importing product widget
 import ProductWidget from '~/components/ProductWidget.vue'
 import ResponsiveImg from '~/components/ResponsiveImage.vue'
+import ProductImg from '~/components/ProductImage.vue'
 
 export default {
   name: 'Home',
   components: {
     ProductWidget,
-    ResponsiveImg
+    ResponsiveImg,
+    ProductImg
   },
   head () {
     return {
