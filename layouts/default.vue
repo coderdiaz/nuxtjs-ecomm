3e67a5954ef575fd04bbbbde03811fb26ff74e4a
Tom Dale <thomas.dale2@gmail.com>
2020-08-28 01:17:42 -0700

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,16 +1,22 @@
 <template>
-  <div>
-    <nuxt />
-    <footer-prismic/>
+  <div class="flex justify-center">
+    <div class="max-w-screen-lg flex flex-col min-h-screen w-full">
+      <NavBar />
+      <nuxt />
+      <footer-prismic/>
+    </div>
+    <div hidden id="snipcart" data-api-key="ZGU1MWUxMjYtY2RmYi00MGUxLWE5NTQtMWM1NzFhODhhYzhhNjM3MzQxNzE1NzE3NTg5NDg4"></div>
   </div>
 </template>
 
 <script>
+import NavBar from '~/components/NavBar.vue'
 import FooterPrismic from '~/components/FooterPrismic.vue'
 
 export default {
   components: {
-    FooterPrismic
+    FooterPrismic,
+    NavBar
   },
   head () {
     return {
