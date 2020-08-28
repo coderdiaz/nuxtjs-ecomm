292afa9fd4abca35561151ea6027c73305c77cba
Tom Dale <thomas.dale2@gmail.com>
2020-08-28 09:04:38 -0700

    Create ProductImage.vue

---
+++ components/ProductImage.vue
@@ -0,0 +1,30 @@
+<template>
+  <ix-img
+    :src="imageSrc()"
+    width="380"
+    height="380"
+    :alt="imgobject.alt"
+    loading="lazy"
+    fixed
+  />
+  <!-- imgixParams="{ fit: 'fill', fill: 'solid', fill-color: 'white'}" -->
+</template>
+
+<script>
+import VueImgix from '~/plugins/vue-imgix.js'
+
+export default {
+  props: {
+    imgobject: {
+      type: Object,
+      default: null
+    }
+  },
+  name: 'ProductImg',
+  methods: {
+    imageSrc() {
+      return (new URL(this.imgobject.url)).pathname
+    }
+  }
+}
+</script>
