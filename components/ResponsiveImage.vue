511d4814afd8c327e1ce97e0c0beb3fa1b7cdf81
Tom Dale <thomas.dale2@gmail.com>
2020-08-28 01:23:37 -0700

    Create ResponsiveImage.vue

---
+++ components/ResponsiveImage.vue
@@ -0,0 +1,30 @@
+<template>
+  <ix-img
+    :src="imageSrc()"
+    :sizes="sizes"
+    :alt="imgobject.alt"
+  />
+</template>
+
+<script>
+import VueImgix from '~/plugins/vue-imgix.js'
+
+export default {
+  props: {
+    imgobject: {
+      type: Object,
+      default: null
+    },
+    sizes: {
+      type: String,
+      default: null
+    }
+  },
+  name: 'ResponsiveImg',
+  methods: {
+    imageSrc() {
+      return (new URL(this.imgobject.url)).pathname
+    }
+  }
+}
+</script>
